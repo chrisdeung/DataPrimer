475772f85316eee4ecb00e89af1ce07bc248b4b9
chrisdeung <deung.chris@gmail.com>
2016-06-28 16:11:17 +0100

    Add error handling
    Change from next(f) to try/except error handling

--- data.py
+++ data.py
@@ -3,10 +3,13 @@ import sys
 f = open("Data.csv", "r")
 
 balance = 0
-next(f)
+#next(f)
 for line in f:
-	entry = line.strip("\n").split(",")
-	balance += int(entry[4])
+	try:
+		entry = line.strip("\n").split(",")
+		balance += int(entry[4])
+	except:
+		pass
 
 print "Balance: " + str(balance)
 
