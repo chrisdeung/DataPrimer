1c509f6cde3bc0280f86e058be361a01dd617124
chrisdeung <deung.chris@gmail.com>
2016-06-29 17:05:04 +0100

    Changed to insert transaction values

--- data_loader.py
+++ data_loader.py
@@ -6,14 +6,13 @@ db = MySQLdb.connect(host="localhost", user="root", passwd="fh3h9f444yKbeo433", 
 #Create cursor object to allow you to execute queries
 cur = db.cursor()
 
-cur.execute("SELECT * FROM info")
-for row in cur.fetchall():
-	print row
-
 f = open("Data.csv", "r")
+next(f)
 
 for line in f:
 	entry = line.strip("\n").split(",")
-	print entry
-	
+	query = "INSERT INTO transaction(date, name, cost, paid, balance, status) VALUES ('" + entry[0] + "', '" + entry[1] + "', '" + entry[2] + "', '" + entry[3] + "', '" + entry[4] + "', '" + entry[5] + "')"
+	cur.execute(query)
+
+db.commit()
 f.close()
