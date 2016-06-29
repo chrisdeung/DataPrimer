2392f4f0860d67ee2fa310d6f0953dd1cb1b4aa9
chrisdeung <deung.chris@gmail.com>
2016-06-29 09:48:06 +0100

    Updated data.py
    Now always includes grand totals

--- data.py
+++ data.py
@@ -6,6 +6,10 @@ analyse = sys.argv[1]
 cost = 0
 paid = 0
 balance = 0
+
+analyse_cost = 0
+analyse_paid = 0
+analyse_balance = 0
 clientList = []
 #next(f)
 
@@ -13,26 +17,31 @@ for line in f:
 	try:
 		entry = line.strip("\n").split(",")
 		
-		if analyse == "all":
-			cost += int(entry[2])
-			paid += int(entry[3])
-			balance += int(entry[4])
-		elif entry[5] == analyse:
-			cost += int(entry[2])
-			paid += int(entry[3])
-			balance += int(entry[4])
+		cost += int(entry[2])
+		paid += int(entry[3])
+		balance += int(entry[4])
+		
+		if entry[5] == analyse:
+			analyse_cost += int(entry[2])
+			analyse_paid += int(entry[3])
+			analyse_balance += int(entry[4])
 			clientList.append(entry[1])
 	except:
 		pass
 
-if analyse != "all":
-	print "\n" + str(len(clientList)) + " client(s) are " + analyse + ":"
-	for client in clientList:
-		print client
-	
-print "\nTotals for " + analyse + " entries:"
+print "\nGrand totals:"
 print "Cost: " + str(cost)
 print "Paid: " + str(paid)
 print "Balance: " + str(balance)
+		
+print "\n" + str(len(clientList)) + " client(s) are " + analyse + ":"
+for client in clientList:
+	print client
+
+if len(clientList) > 0:		
+	print "\nTotals for " + analyse + " entries:"
+	print "Cost: " + str(analyse_cost)
+	print "Paid: " + str(analyse_paid)
+	print "Balance: " + str(analyse_balance)
 
 f.close()
