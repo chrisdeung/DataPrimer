9c50a1a0428295986dbacc68ca869d66b11634cd
chrisdeung <deung.chris@gmail.com>
2016-06-29 10:53:28 +0100

    Updated data.py so doesn't use status
    Added function as well

--- data.py
+++ data.py
@@ -1,3 +1,8 @@
+def calcAnalysis(record):
+	#cost, paid, balance
+	return [int(record[2]), int(record[3]), int(record[4])]
+
+### MAIN PROGRAM ###
 import sys
 
 f = open("Data.csv", "r")
@@ -7,10 +12,9 @@ cost = 0
 paid = 0
 balance = 0
 
-analyse_cost = 0
-analyse_paid = 0
-analyse_balance = 0
-clientList = []
+#closed, open, pending -- cost, paid, balance
+analysis = [[0,0,0],[0,0,0],[0,0,0]]
+clientList = [[],[],[]]
 #next(f)
 
 for line in f:
@@ -21,11 +25,15 @@ for line in f:
 		paid += int(entry[3])
 		balance += int(entry[4])
 		
-		if entry[5] == analyse:
-			analyse_cost += int(entry[2])
-			analyse_paid += int(entry[3])
-			analyse_balance += int(entry[4])
-			clientList.append(entry[1])
+		if int(entry[4])==0:
+			analysis[0] = [sum(x) for x in zip(analysis[0], calcAnalysis(entry))]
+			clientList[0].append(entry[1])			
+		elif int(entry[3])==0:
+			analysis[2] = [sum(x) for x in zip(analysis[2], calcAnalysis(entry))]
+			clientList[2].append(entry[1])			
+		else:
+			analysis[1] = [sum(x) for x in zip(analysis[1], calcAnalysis(entry))]
+			clientList[1].append(entry[1])			
 	except:
 		pass
 
@@ -33,15 +41,26 @@ print "\nGrand totals:"
 print "Cost: " + str(cost)
 print "Paid: " + str(paid)
 print "Balance: " + str(balance)
-		
-print "\n" + str(len(clientList)) + " client(s) are " + analyse + ":"
-for client in clientList:
+
+index = 0
+if analyse=="closed":
+	index = 0
+elif analyse=="open":
+	index = 1
+elif analyse=="pending":
+	index = 2
+else:
+	raise Exception("Incorrect argument for analysis!")
+	
+
+print "\n" + str(len(clientList[index])) + " client(s) are " + analyse + ":"
+for client in clientList[index]:
 	print client
 
-if len(clientList) > 0:		
+if len(clientList[index]) > 0:		
 	print "\nTotals for " + analyse + " entries:"
-	print "Cost: " + str(analyse_cost)
-	print "Paid: " + str(analyse_paid)
-	print "Balance: " + str(analyse_balance)
+	print "Cost: " + str(analysis[index][0])
+	print "Paid: " + str(analysis[index][1])
+	print "Balance: " + str(analysis[index][2])
 
 f.close()
